51ddd0eea42abd3497c97e5d31dab3aa4861ee4e
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-10-16 14:02:41 +0700

    add: enum data

---
+++ src/data/reminderType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MuslimahGuide\data;
+
+class reminderType
+{
+    const start = "period_start";
+    const end = "period_end";
+    const late = "period_late";
+}
---
+++ src/data/role.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MuslimahGuide\data;
+
+class role
+{
+    const admin = "admin";
+    const user = "user";
+}
