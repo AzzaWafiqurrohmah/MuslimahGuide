4c01c1bdb87d51cacc2957d1086ecdb3dfec5f9d
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-10-31 14:01:16 +0700

    add: middleware test

---
+++ tests/Middleware/MustLogin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MuslimahGuide\Middleware;
+
+use MuslimahGuide\Config\database;
+use MuslimahGuide\data\role;
+use MuslimahGuide\Domain\session;
+use MuslimahGuide\Domain\user;
+use MuslimahGuide\Repository\SessionRepository;
+use MuslimahGuide\Repository\UserRepository;
+use MuslimahGuide\Service\sessionService;
+use PHPUnit\Framework\TestCase;
+
+class MustLogin extends TestCase
+{
+    private mustLoginMiddleware $middleware;
+    private UserRepository $userRepo;
+    private SessionRepository $sessionRepo;
+
+    protected function setUp(): void
+    {
+        $this->middleware = new mustLoginMiddleware();
+
+        $this->userRepo = new UserRepository(database::getConnection());
+        $this->sessionRepo = new SessionRepository(database::getConnection());
+    }
+
+    public function testBeforeGuest()
+    {
+        $this->middleware->before();
+        $this->expectOutputString("[Location: /users/login]");
+    }
+
+    public function testBeforeLoginUser(){
+        $user = new user(null, null, role::admin,"082342123456", null, "azza23", "rahasia");
+        $user->setId($this->userRepo->addAll($user));
+
+        $session = new session(uniqid(), $user);
+        $this->sessionRepo->save($session);
+
+        $cookieName = sessionService::$COOKIE_NAME;
+        $_COOKIE[$cookieName] = $session->getId();
+
+
+        $this->middleware->before();
+        $this->expectOutputString("");
+    }
+
+}
