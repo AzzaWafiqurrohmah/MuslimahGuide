2df41887934bcc6a4577080b93afd6927e8872e1
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-11-01 16:07:53 +0700

    add: landing page example

--- src/view/landingPage.php
+++ src/view/landingPage.php
@@ -1,9 +1,13 @@
 <!-- Template Main CSS File -->
 <!--<link href="assets/css/style.css" rel="stylesheet">-->
+
+<title>Landing page</title>
 </head>
 
 <body>
     <main>
-        <h1>Landing page</h1>
-<!--        <input type="submit">-->
+        <form method="post" action="/">
+            <h1>Landing page</h1>
+            <input type="submit" id="login" name="login" value="login">
+        </form>
     </main>
