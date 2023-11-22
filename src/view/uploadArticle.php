89777f76e65fea392fd02b54504b0f5b56e3b878
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-11-22 11:49:11 +0700

    docs: change name to ZenFemina

--- src/view/uploadArticle.php
+++ src/view/uploadArticle.php
@@ -10,7 +10,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="/dashboard" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">MuslimahGuide</span>
+        <span class="d-none d-lg-block">ZenFemina</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
