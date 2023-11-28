437835a4af8df10c0b647e9194644dc33a6ae001
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-11-28 18:17:38 +0700

    add:profile api

--- src/controller/api/profile.php
+++ src/controller/api/profile.php
@@ -7,16 +7,20 @@ use MuslimahGuide\data\role;
 use MuslimahGuide\Domain\user;
 use MuslimahGuide\Repository\SessionRepository;
 use MuslimahGuide\Repository\UserRepository;
+use MuslimahGuide\Service\sessionService;
 
 class profile
 {
     private UserRepository $userRepo;
     private SessionRepository $sessionRepo;
+    private sessionService $sessionService;
 
     public function __construct()
     {
         $this->userRepo = new UserRepository(database::getConnection());
         $this->sessionRepo = new SessionRepository(database::getConnection());
+
+        $this->sessionService = new sessionService($this->sessionRepo, $this->userRepo);
     }
 
     public function get_profile(){
@@ -80,6 +84,69 @@ class profile
             );
         }
 
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function put_cycle(){
+
+    }
+
+    public function get_cycle(){
+
+    }
+
+    public function post_password(){
+        $token = $_POST['token'];
+        $pass = $_POST['password'];
+        $response = [];
+
+        $session = $this->sessionRepo->getById($token);
+
+        if($session !== null){
+            $user_id = $session->getUserId()->getId();
+
+            $user = $this->userRepo->getById($user_id);
+            $user->setPassword($pass);
+            if($this->userRepo->update($user)){
+                $response = array(
+                    'status' => 1,
+                    'message' => "Password berhasil diperbarui"
+                );
+            } else {
+                $response = array(
+                    'status' => 0,
+                    'message' => "Password Gagal diperbarui"
+                );
+            }
+        } else {
+            $response = array(
+                'status' => 0,
+                'message' => "Token tidak ditemukan"
+            );
+        }
+
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function signOut(){
+        $token = $_POST['token'];
+        $this->sessionRepo->deleteById($token);
+
+        $res = $this->sessionRepo->findById($token);
+        if($res == null){
+            $response = array(
+                'status' => 1,
+                'message' => "Anda berhasil keluar"
+            );
+        } else {
+            $response = array(
+                'status' => 0,
+                'message' => "Token anda tidak sesuai"
+            );
+        }
         header('Content-Type: application/json');
         echo json_encode($response);
     }
